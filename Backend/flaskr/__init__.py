5a243a0bf0f2779a432729bbe793d198e8897216
Redevilkz <stefano.badalucco@gmail.com>
2024-03-19 22:26:35 +0100

    Updated flask backend

--- Backend/flaskr/__init__.py
+++ Backend/flaskr/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-  
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -29,5 +28,15 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
