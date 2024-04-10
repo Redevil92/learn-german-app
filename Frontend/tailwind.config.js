c21b30799911e82db25a76eef9b7cbd0022ecf5a
Redevilkz <stefano.badalucco@gmail.com>
2024-04-10 22:12:34 +0200

    Added default values in Tailwind configuration.

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {},
+    extend: {
+      colors: {
+        "error-color": "#c34c4c",
+        "font-color": "#1f1f1f",
+        "font-color-light": "#5e5e5e",
+        "neuter-color": "#a4a4a4",
+        "masculin-color": "#4c8ac3",
+        "feminine-color": "#c34c4c",
+      },
+      fontSize: {
+        sm: "0.75rem",
+        base: "0.875rem",
+        lg: "1.065rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
+      },
+    },
   },
   plugins: [],
 };
