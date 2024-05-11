b5d2834329a8c2e70c70a1124d7c0b5536fe34e5
Redevil92 <stefano.badalucco@gmail.com>
2024-05-11 13:40:04 +0200

    Added verbs file to create VerbZeitform

---
+++ Frontend/src/GermanGrammar/Verbs.ts
@@ -0,0 +1,157 @@
+import Verb from "../models/Verb";
+import VerbZeitform from "../models/VerbZeitform";
+
+const werdenPraesens: VerbZeitform = {
+    zeitform: "Präsens",
+    ich: "werde",
+    du: "wirst",
+    er: "wird",
+    wir: "werden",
+    ihr: "werdet",
+    sie: "werden",
+};
+
+const habenPraesens: VerbZeitform = {
+    zeitform: "Präsens",
+    ich: "habe",
+    du: "hast",
+    er: "hat",
+    wir: "haben",
+    ihr: "habt",
+    sie: "haben",
+};
+
+const seinPraesens: VerbZeitform = {
+    zeitform: "Präsens",
+    ich: "bin",
+    du: "bist",
+    er: "ist",
+    wir: "sind",
+    ihr: "seid",
+    sie: "sind",
+};
+
+const habenPraeteritum: VerbZeitform = {
+    zeitform: "Präteritum",
+    ich: "hatte",
+    du: "hattest",
+    er: "hatte",
+    wir: "hatten",
+    ihr: "hattet",
+    sie: "hatten",
+};
+
+const seinPraeteritum: VerbZeitform = {
+    zeitform: "Präteritum",
+    ich: "war",
+    du: "warst",
+    er: "war",
+    wir: "waren",
+    ihr: "wart",
+    sie: "waren",
+};
+
+
+const getVerbRoot = (verb: Verb): string => {
+    return verb.infinitive.slice(0, verb.infinitive.length - 2);
+}
+
+const getHilfsverbPraesens = (verb: Verb): VerbZeitform => {
+    console.log(verb.hilfsverb.trim() === "haben", verb.hilfsverb, 'haben');
+    if (verb.hilfsverb.trim() === "haben") {
+        return habenPraesens;
+    } else {
+        return seinPraesens;
+    }
+}
+
+const getHilfsverbPrateritum = (verb: Verb): VerbZeitform => {
+    if (verb.hilfsverb.trim() === "haben") {
+        return habenPraeteritum;
+    } else {
+        return seinPraeteritum;
+    }
+}
+
+export const getPraesens = (verb: Verb): VerbZeitform => {
+    const root = getVerbRoot(verb);
+
+    return {
+        zeitform: "Präsens",
+        ich: verb.praesens_ich,
+        du: verb.praesens_du,
+        er: verb.praesens_er,
+        wir: root + "en",
+        ihr: root + "t",
+        sie: root + "en",
+    };
+}
+
+export const getPraeteritum = (verb: Verb): VerbZeitform => {
+    const root = getVerbRoot(verb);
+
+    return {
+        zeitform: "Präteritum",
+        ich: verb.praeteritum_ich,
+        du: root + "st",
+        er: root + "t",
+        wir: root + "en",
+        ihr: root + "t",
+        sie: root + "en",
+    };
+}
+
+export const getFuturI = (verb: Verb): VerbZeitform => {
+    return {
+        zeitform: "Futur I",
+        ich: werdenPraesens.ich + " " + verb.infinitive,
+        du: werdenPraesens.du + " " + verb.infinitive,
+        er: werdenPraesens.er + " " + verb.infinitive,
+        wir: werdenPraesens.wir + " " + verb.infinitive,
+        ihr: werdenPraesens.ihr + " " + verb.infinitive,
+        sie: werdenPraesens.sie + " " + verb.infinitive,
+    };
+}
+
+export const getPerfekt = (verb: Verb): VerbZeitform => {
+    const hilfsverbPraesens = getHilfsverbPraesens(verb);
+    console.log(hilfsverbPraesens);
+
+    return {
+        zeitform: "Perfekt",
+        ich: hilfsverbPraesens.ich + " " + verb.partizip_II,
+        du: hilfsverbPraesens.du + " " + verb.partizip_II,
+        er: hilfsverbPraesens.er + " " + verb.partizip_II,
+        wir: hilfsverbPraesens.wir + " " + verb.partizip_II,
+        ihr: hilfsverbPraesens.ihr + " " + verb.partizip_II,
+        sie: hilfsverbPraesens.sie + " " + verb.partizip_II,
+    };
+}
+
+export const getPlusquamperfekt = (verb: Verb): VerbZeitform => {
+    const hilfsverbPrateritum = getHilfsverbPrateritum(verb);
+
+    return {
+        zeitform: "Plusquamperfekt",
+        ich: hilfsverbPrateritum.ich + " " + verb.partizip_II,
+        du: hilfsverbPrateritum.du + " " + verb.partizip_II,
+        er: hilfsverbPrateritum.er + " " + verb.partizip_II,
+        wir: hilfsverbPrateritum.wir + " " + verb.partizip_II,
+        ihr: hilfsverbPrateritum.ihr + " " + verb.partizip_II,
+        sie: hilfsverbPrateritum.sie + " " + verb.partizip_II,
+    };
+}
+
+export const getFuturII = (verb: Verb): VerbZeitform => {
+    const hilfsverbPraesens = getHilfsverbPraesens(verb);
+
+    return {
+        zeitform: "Futur II",
+        ich: hilfsverbPraesens.ich + " " + verb.partizip_II + " " + verb.hilfsverb,
+        du: hilfsverbPraesens.du + " " + verb.partizip_II + " " + verb.hilfsverb,
+        er: hilfsverbPraesens.er + " " + verb.partizip_II + " " + verb.hilfsverb,
+        wir: hilfsverbPraesens.wir + " " + verb.partizip_II + " " + verb.hilfsverb,
+        ihr: hilfsverbPraesens.ihr + " " + verb.partizip_II + " " + verb.hilfsverb,
+        sie: hilfsverbPraesens.sie + " " + verb.partizip_II + " " + verb.hilfsverb,
+    };
+}
