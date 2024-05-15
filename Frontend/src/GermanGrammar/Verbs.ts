5a858189101ea48935c198ac8612da5f8a26db0a
Redevil92 <stefano.badalucco@gmail.com>
2024-05-15 23:12:04 +0200

    Added logic for separable verbs.

--- Frontend/src/GermanGrammar/Verbs.ts
+++ Frontend/src/GermanGrammar/Verbs.ts
@@ -72,17 +72,23 @@ const getHilfsverbPrateritum = (verb: Verb): VerbZeitform => {
     }
 }
 
+const getSeparableVerbPrefixIfAny = (verb: string): string => {
+    return verb.split(' ').length > 0 ? verb.split(' ')[1] : '';
+}
+
 export const getPraesens = (verb: Verb): VerbZeitform => {
     const root = getVerbRoot(verb);
+
+    const separablePrefix = getSeparableVerbPrefixIfAny(verb.praesens_ich);
 
     return {
         zeitform: "Präsens",
         ich: verb.praesens_ich,
         du: verb.praesens_du,
         er: verb.praesens_er,
-        wir: root + "en",
-        ihr: root + "t",
-        sie: root + "en",
+        wir: root.replace(separablePrefix, '') + "en" + (separablePrefix ? ' ' + separablePrefix : ''),
+        ihr: root.replace(separablePrefix, '') + "t" + (separablePrefix ? ' ' + separablePrefix : ''),
+        sie: root.replace(separablePrefix, '') + "en" + (separablePrefix ? ' ' + separablePrefix : ''),
     };
 }
 
@@ -93,20 +99,27 @@ export const getPraeteritum = (verb: Verb): VerbZeitform => {
     }
 
     const weakVerb = root.endsWith("t") || root.endsWith("d") || root.endsWith("m") || root.endsWith("n") || root.endsWith("e") || root.endsWith('te');
-    const shouldAddE = !weakVerb && !verb.praeteritum_ich.endsWith('e');
+    const verbWithoutPrefix = verb.praeteritum_ich.split(' ').length > 0 ? verb.praeteritum_ich.split(' ')[0]  : verb.praeteritum_ich;
+    const shouldAddE = !weakVerb && !verbWithoutPrefix.endsWith('e');
+
+    const separablePrefix = getSeparableVerbPrefixIfAny(verb.praeteritum_ich);
+
+    const praeteritum_root = verbWithoutPrefix + (shouldAddE? 'e' : '');
+
 
     return {
         zeitform: "Präteritum",
         ich: verb.praeteritum_ich,
-        du: verb.praeteritum_ich + (shouldAddE ? 'e' : '') + "st",
+        du: praeteritum_root + "st" + (separablePrefix ? ' ' + separablePrefix : ''),
         er: verb.praeteritum_ich,
-        wir: verb.praeteritum_ich + (shouldAddE? 'e' : '') +"n",
-        ihr: verb.praeteritum_ich +  (shouldAddE ? 'e' : '')+"t",
-        sie: verb.praeteritum_ich + (shouldAddE? 'e': '') + "n",
+        wir: praeteritum_root + "n" + (separablePrefix ? ' ' + separablePrefix : ''),
+        ihr: praeteritum_root + "t" + (separablePrefix ? ' ' + separablePrefix : ''),
+        sie: praeteritum_root + "n" + (separablePrefix ? ' ' + separablePrefix : ''),
     };
 }
 
 export const getFuturI = (verb: Verb): VerbZeitform => {
+    console.log(verb)
     return {
         zeitform: "Futur I",
         ich: werdenPraesens.ich + " " + verb.infinitive,
